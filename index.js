c16c148539660a9a0e41686ace8e5fe8a5230416
hector <e3.corefloriambra@proton.me>
2024-06-21 12:47:23 -0500

    se cambia el puerto de 5000 a 8080

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.use(router);
 
-const listen = process.env.PORT ?? 5000;
+const listen = process.env.PORT ?? 8080;
 
 app.listen(listen, () => {
     console.log( `SERVER RUNNING PORT ${listen}` )

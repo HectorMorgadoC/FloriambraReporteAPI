9c099e9d983dce58a248410ae88191c709b56858
hector <e3.corefloriambra@proton.me>
2024-06-21 08:02:30 -0500

    se agrega el middleware-proxy

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ import { json } from "express";
 import express,{ json as _json } from "express";
 import { router } from "./Router/routers.js";
 import  cors  from "cors";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
-
 
 app.use(express.json({type:'*/*'}))
 
 app.use(cors({
-    origin:'https://hectormorgadoc.github.io/ReporteFlorambraC/',
+    origin:'*',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use('/',createProxyMiddleware({
+    target: 'https://floriambra-reporte-api.vercel.app/',
+    changeOrigin: true,
+    pathRewrite: {
+        'ʌ/': ''
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        res.setHeader('Access-Control-Allow-Origin','*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
+    }
+
+}))
+
 app.use(router);
 
 const listen = process.env.PORT ?? 5000;
